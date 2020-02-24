8a4a7db6582d2a06629730ef8bcdfa3202040522
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-02-24 18:17:12 +0600

    binding

--- src/app/blog-post-title/blog-post-title.component.ts
+++ src/app/blog-post-title/blog-post-title.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPostTitleComponent implements OnInit {
 
+  title: string;
+  summary:  string;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.title = "Blog  Title";
+    this.summary = "Blog Post Summary";
+    
   }
 
 }

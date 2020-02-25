1bd628e5323ee38c859918f2baf19e2a63359f83
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-02-25 13:50:42 +0600

    Calling pipe inside Angular components

--- src/app/blog-post-title/blog-post-title.component.ts
+++ src/app/blog-post-title/blog-post-title.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BlogPost } from '../blog-post';
+import { TruncatePipe } from '../truncate.pipe';
 
 @Component({
   selector: 'app-blog-post-title',
@@ -10,10 +11,11 @@ export class BlogPostTitleComponent implements OnInit {
 
   @Input() post: BlogPost;
 
-  constructor() { }
+  constructor(private  truncatePipe: TruncatePipe) { }
 
   ngOnInit() {
 
+this.post.summary = this.truncatePipe.transform(this.post.summary, 30);
     
   }
 
